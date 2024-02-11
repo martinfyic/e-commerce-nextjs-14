07bfc6024176b6ef0cefd3c49c89796c7941796b
Martin Ferreira <martin.f.yic@gmail.com>
2024-02-11 18:15:32 -0300

    refactor: :lipstick: Implemente componente Title y ProductGrid

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,14 @@
-import { titleFont } from '@/config/fonts';
+import { ProductGrid, Title } from '@/components';
+import { initialData } from '@/seed/seed';
+
+const products = initialData.products;
 
 export default function Home() {
-	return (
-		<>
-			<h1>Hola mundo</h1>
-			<h1 className={titleFont.className}>Hola mundo</h1>
-		</>
-	);
+  return (
+    <>
+      <Title title='Market' subTitle='All the products' className='mb-2' />
+
+      <ProductGrid products={products} />
+    </>
+  );
 }
