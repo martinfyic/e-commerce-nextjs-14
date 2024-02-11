d48cc10dff7309d4b2fd1424bcf9c283c93140fd
Martin Ferreira <martin.f.yic@gmail.com>
2024-02-11 18:19:11 -0300

    feat: :label: agrego interface para productos

---
+++ src/interfaces/products.interface.ts
@@ -0,0 +1,15 @@
+export interface Product {
+  description: string;
+  images: string[];
+  inStock: number;
+  price: number;
+  sizes: ValidSizes[];
+  slug: string;
+  tags: string[];
+  title: string;
+  type: ValidTypes;
+  gender: 'men' | 'women' | 'kid' | 'unisex';
+}
+
+export type ValidSizes = 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL' | 'XXXL';
+export type ValidTypes = 'shirts' | 'pants' | 'hoodies' | 'hats';
