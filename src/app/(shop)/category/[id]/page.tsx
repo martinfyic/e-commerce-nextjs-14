0edbeb201167b53010386823dfecd54444ec4a5b
Martin Ferreira <martin.f.yic@gmail.com>
2024-02-11 18:16:50 -0300

    refactor: :zap: agregue logia para mostrar categrias

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,4 +1,20 @@
-export default function CategoryPage() {
+import { notFound } from 'next/navigation';
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+const categories = ['men', 'women', 'kids'];
+
+export default function CategoryPage({ params }: Props) {
+  const { id } = params;
+
+  if (!categories.includes(id)) {
+    notFound();
+  }
+
   return (
     <div>
       <h1>Category Page</h1>
