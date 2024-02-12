1961e950ed914f64daf87a6e23592625411bf9e7
Martin Ferreira <martin.f.yic@gmail.com>
2024-02-12 14:40:16 -0300

    refactor: :lipstick: implemento detalle de producto en pagina del producto

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,51 @@
-export default function ProductPage() {
+import { notFound } from 'next/navigation';
+
+import { initialData } from '@/seed/seed';
+import { titleFont } from '@/config/fonts';
+import { QuantitySelector, SizeSelector } from '@/components/product';
+
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export default function ProductPage({ params }: Props) {
+  const { slug } = params;
+
+  const product = initialData.products.find((prod) => prod.slug === slug);
+
+  if (!product) {
+    notFound();
+  }
+
   return (
-    <div>
-      <h1>Product Page</h1>
+    <div className='mb-20 mt-5 grid grid-cols-1 gap-3 md:grid-cols-3'>
+      {/* Slideshow */}
+      <div className='col-span-1 md:col-span-2'>
+        <h1>{product.title}</h1>
+      </div>
+
+      {/* Detalle */}
+      <div className='col-span-1 px-5'>
+        <h1 className={`${titleFont.className} text-xl font-bold antialiased`}>
+          {product.title}
+        </h1>
+        <p className='mb-5 text-lg'>${product.price}</p>
+
+        {/* Selector de talla */}
+        <SizeSelector availableSizes={product.sizes} selectedSize={product.sizes[0]} />
+
+        {/* Selector de cantidad */}
+        <QuantitySelector quantity={3} />
+
+        {/* Botón agregar al carrito */}
+        <button className='btn-primary my-5'>Add to cart</button>
+
+        {/* Descripción */}
+        <h3 className='text-sm font-bold'>Descripción</h3>
+        <p className='font-light'>{product.description}</p>
+      </div>
     </div>
   );
 }
