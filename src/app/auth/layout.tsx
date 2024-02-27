52fcfbea0b28c9f9209f43b0086bd989ee07fcdc
Martin Ferreira <martin.f.yic@gmail.com>
2024-02-26 21:57:41 -0300

    refactor: :passport_control: creo control para usuario si no esta registrado

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,4 +1,12 @@
-export default function ShopLayout({ children }: { children: React.ReactNode }) {
+import { redirect } from 'next/navigation';
+
+import { auth } from '../../auth.config';
+
+export default async function ShopLayout({ children }: { children: React.ReactNode }) {
+  const session = await auth();
+
+  if (session?.user) redirect('/');
+
   return (
     <main className='flex justify-center'>
       <div className='w-full px-10 sm:w-[350px]'>{children}</div>
