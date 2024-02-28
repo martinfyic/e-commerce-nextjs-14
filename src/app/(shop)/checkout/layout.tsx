5cce12c30c46af923ab3ea2ae43ef9650874cab5
Martin Ferreira <martin.f.yic@gmail.com>
2024-02-28 20:03:05 -0300

    fix: :bug: corrijo layout lo dejo a modo de ejemplo para proteger rutas sin middleware

--- src/app/(shop)/checkout/layout.tsx
+++ src/app/(shop)/checkout/layout.tsx
@@ -3,14 +3,14 @@
 // import { auth } from '@/auth.config';
 // import { redirect } from 'next/navigation';
 
-// export default async function CheckoutLayout({
-//   children,
-// }: {
-//   children: React.ReactNode;
-// }) {
-//   const session = await auth();
+export default async function CheckoutLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  // const session = await auth();
 
-//   if (!session?.user) redirect('/auth/login?redirectTo=/checkout/address');
+  // if (!session?.user) redirect('/auth/login?redirectTo=/checkout/address');
 
-//   return <>{children}</>;
-// }
+  return <>{children}</>;
+}
