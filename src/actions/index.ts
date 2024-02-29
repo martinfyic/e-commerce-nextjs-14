4f1c8f936e2af7b391472fedb00aa85011e54886
Martin Ferreira <martin.f.yic@gmail.com>
2024-02-29 20:27:00 -0300

    chore: :art: archivos de exportacion

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,6 +2,8 @@ export * from './auth/login';
 export * from './auth/logout';
 export * from './auth/newAccount';
 
+export * from './country/get-countries';
+
 export * from './products/get-product-by-slug';
 export * from './products/get-stock-by-slug';
 export * from './products/product-pagination';
