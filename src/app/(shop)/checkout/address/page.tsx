09ed4bbb340340e0edbf078bf1dab33ce4e7b21b
Martin Ferreira <martin.f.yic@gmail.com>
2024-03-02 16:51:57 -0300

    refactor: :recycle: implemento auth session para obtener direccion de base de dato por el id de usuario

--- src/app/(shop)/checkout/address/page.tsx
+++ src/app/(shop)/checkout/address/page.tsx
@@ -1,6 +1,7 @@
 import { Title } from '@/components';
 import { AddressForm } from './ui/AddressForm';
-import { getCountries } from '@/actions';
+import { getCountries, getUserAddress } from '@/actions';
+import { auth } from '@/auth.config';
 
 export const metadata = {
   title: 'Shipping Address',
@@ -9,12 +10,25 @@ export const metadata = {
 
 export default async function AddressPage() {
   const countries = await getCountries();
+
+  const session = await auth();
+
+  if (!session?.user) {
+    return (
+      <h3 className='m-2 bg-red-500 p-2 text-center text-5xl text-red-200'>
+        There is no user session
+      </h3>
+    );
+  }
+
+  const userAddress = (await getUserAddress(session.user.id)) ?? undefined;
+
   return (
     <div className='mb-72 flex flex-col px-10 sm:items-center sm:justify-center sm:px-0'>
       <div className='flex  w-full flex-col justify-center text-left xl:w-[1000px]'>
         <Title title='Address' subTitle='Shipping address' />
 
-        <AddressForm countries={countries} />
+        <AddressForm countries={countries} userStoreAddress={userAddress} />
       </div>
     </div>
   );
