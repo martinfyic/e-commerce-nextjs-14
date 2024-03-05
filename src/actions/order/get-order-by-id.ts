88bfc5d9705474f1ac9fd35ffefcf8e6acb9dc4b
Martin Ferreira <martin.f.yic@gmail.com>
2024-03-05 18:03:54 -0300

    feat: :sparkles: creo server action para obtener ordenes por id de db

---
+++ src/actions/order/get-order-by-id.ts
@@ -0,0 +1,61 @@
+'use server';
+
+import { auth } from '@/auth.config';
+import prisma from '@/lib/prisma';
+
+export const getOrderById = async (id: string) => {
+  try {
+    const order = await prisma.order.findUnique({
+      where: {
+        id: id,
+      },
+      include: {
+        OrderAddress: true,
+        OrderItem: {
+          select: {
+            price: true,
+            quantity: true,
+            size: true,
+
+            product: {
+              select: {
+                title: true,
+                slug: true,
+
+                ProductImage: {
+                  select: {
+                    url: true,
+                  },
+                  take: 1,
+                },
+              },
+            },
+          },
+        },
+      },
+    });
+
+    if (!order) throw new Error(`Error - id ${id} does not exist`);
+
+    const session = await auth();
+    if (!session) throw new Error('There is no user session');
+    if (session.user.role === 'user') {
+      if (session.user.id !== order?.userId) {
+        throw new Error(`The id ${id} does not correspond to the session user`);
+      }
+    }
+
+    console.log(order);
+
+    return {
+      ok: true,
+      order,
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      ok: false,
+      message: 'Error - Talk to administrator',
+    };
+  }
+};
