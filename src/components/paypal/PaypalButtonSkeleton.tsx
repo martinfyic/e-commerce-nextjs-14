32e08cf06b909514a2dbe301e219109cec483fd4
Martin Ferreira <martin.f.yic@gmail.com>
2024-03-07 17:37:03 -0300

    feat: :lipstick: creo skeleton de boton de paypal

---
+++ src/components/paypal/PaypalButtonSkeleton.tsx
@@ -0,0 +1,10 @@
+export const PaypalButtonSkeleton = () => {
+  return (
+    <>
+      <div className='mb-14 animate-pulse'>
+        <div className='h-11 rounded bg-gray-300'></div>
+        <div className='mt-3 h-11 rounded bg-gray-600'></div>
+      </div>
+    </>
+  );
+};
