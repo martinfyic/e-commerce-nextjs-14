87048fdc2af0053edde07bd8e94f72ef741722cb
Martin Ferreira <martin.f.yic@gmail.com>
2024-03-08 14:17:06 -0300

    chore: :art: actualizo archivos de exportacion

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -12,6 +12,7 @@ export * from './order/get-order-by-id';
 export * from './order/get-orders-by-user';
 export * from './order/place-order';
 
+export * from './payments/paypal-check-payment';
 export * from './payments/set-transaction-id';
 
 export * from './products/get-product-by-slug';
